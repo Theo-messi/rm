65a591b513abdf529a497a61f80dacba74ccd343
Theo <fanxiaobin422@gmail.com>
2023-12-04 13:31:19 +0800

    rm

--- .vitepress/configs/head.ts
+++
@@ -1,8 +0,0 @@
-import type { HeadConfig } from 'vitepress'
-
-const isDevelopment = process.env.NODE_ENV === 'development'
-
-export const head: HeadConfig[] = [
-  // LOGO
-  ['link', { rel: 'icon', href: 'https://theovan.wiki/favicon.ico' }]
-]
